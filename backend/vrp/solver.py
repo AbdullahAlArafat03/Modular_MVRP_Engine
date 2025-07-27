a00160c50eab72a754a396bd7b89c0781fec5a41
AbdullahAlArafat03 <s202156290@kfupm.edu.sa>
2025-07-27 17:51:07 +0300

    Update solver.py

--- backend/vrp/solver.py
+++ backend/vrp/solver.py
@@ -4,14 +4,11 @@ from distance_matrix import distance_matrix
 def solve_vrp(data: VRPRequest):
     # Convert to OR-Tools format
     locations = data.locations
-
     demands = data.demands
-    
     capacities = data.vehicle_capacities
-    
     num_vehicles = data.num_vehicles
-    
     depot_index = data.depot_index
+    
     # Solve with constraints
     data_model = {
     "distance_matrix": distance_matrix(locations),
@@ -20,8 +17,8 @@ def solve_vrp(data: VRPRequest):
     "num_vehicles": num_vehicles,
     "depot": depot_index
 }
-
     routing, manager, solution = ortools_solver(data_model)
+    
     # Return list of routes
     routes = get_routes(routing, manager, solution, data["num_vehicles"])
     return {"routes": routes}
