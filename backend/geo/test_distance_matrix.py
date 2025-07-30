a681f2741adaf8ca5bca2716aa6387cebc0b5994
AbdullahAlArafat03 <s202156290@kfupm.edu.sa>
2025-07-30 21:37:14 +0300

    Update test_distance_matrix.py

--- backend/geo/test_distance_matrix.py
+++ backend/geo/test_distance_matrix.py
@@ -1,4 +1,17 @@
+import os
 from distance_matrix import create_distance_matrix
-from distance_matrix import send_request
-from distance_matrix import build_distance_matrix
+from dotenv import load_dotenv
+from vrp.models import VRPRequest
+from vrp.config import API_KEY
 
+load_dotenv()
+API_key = os.getenv("API_KEY")
+
+
+def test(data:VRPRequest){
+
+  locations =  [data.depot] + data.stops
+  distance_matrix = create_distance_matrix(locations, API_KEY)
+  return distance_matrix
+  
+}
