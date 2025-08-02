263d57771d1f15855926682ca6e38b38603b51a1
AbdullahAlArafat03 <s202156290@kfupm.edu.sa>
2025-08-02 12:49:41 +0300

    Update test_solver.py

--- backend/vrp/test_solver.py
+++ backend/vrp/test_solver.py
@@ -9,12 +9,12 @@ API_KEY = os.getenv("API_KEY")
 
 # Create test data
 request_data = VRPRequest(
-    depot=Location(id="depot", coordinates=(40.7580, -73.9855), demand=0),
+    depot=Location(id="depot", coordinates=(40.7580, -73.9855), time_window=(0, 1000), demand=0),
     stops=[
-        Location(id="A", coordinates=(40.730610, -73.935242), demand=5),
-        Location(id="B", coordinates=(40.712776, -74.005974), demand=3),
-        Location(id="C", coordinates=(40.706192, -74.009160), demand=2),
-        Location(id="D", coordinates=(40.748817, -73.985428), demand=4)
+        Location(id="A", coordinates=(40.730610, -73.935242), time_window=(0, 1000), demand=5),
+        Location(id="B", coordinates=(40.712776, -74.005974), time_window=(0, 1000), demand=3),
+        Location(id="C", coordinates=(40.706192, -74.009160), time_window=(0, 1000), demand=2),
+        Location(id="D", coordinates=(40.748817, -73.985428), time_window=(0, 1000), demand=4)
     ],
     vehicles=[
         Vehicle(id="v1", capacity=10, type="van"),
